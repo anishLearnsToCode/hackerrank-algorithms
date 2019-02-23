6ab2a057f032b4d169d941fe375e62023c675460
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-02-23 15:01:02 +0100

    solved organizing containers of balls

--- src/OrganizingContainersOfBalls.java
+++ src/OrganizingContainersOfBalls.java
@@ -1,54 +1,96 @@
+// https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem?h_r=next-challenge&h_v=zen
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class OrganizingContainersOfBalls {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner  s= new Scanner(System.in);
-        int size, i, j;
-        String ans;
-
-        System.out.print("Enter the size : ");
-        size = s.nextInt();
-
-        long arr[][] = new long[size][size];
-        long container[] =  new long [size];
-
-        for(i=0 ; i<container.length ; container[i]=0, i++);
-
-        for(i=0 ; i<arr.length ; i++){
-            for(j=0, container[i]=0 ; j<arr.length ; j++){
-                arr[i][j] = s.nextInt();
-                if(arr[i][j]!=0){
-                    container[i] += arr[i][j];
-                }
-            }
-        }
-
-        ans = swapping(container, arr);
+        int size = scanner.nextInt();
+        int[][] container = new int[size][size];
+        input(container);
+//        print(container);
+        String ans = isPossibleToOrganizeContainer(container, size);
         System.out.println(ans);
     }
 
-    public static String swapping(long container[], long arr[][]){
-        int i, k, j, n=arr.length;
-        boolean flag;
-        long sum, extra;
-        String ans;
+    private static String isPossibleToOrganizeContainer(int[][] container, int size) {
+        HashMap<Long, Integer> rowSums = getRowSums(container, size);
+        HashMap<Long, Integer> columnSums = getColumnSums(container, size);
 
-        for(i=0, extra=0 ; i<n ; i++){
-            for(j=0, sum=0 ; j<n ; j++){
-                if(j==i)
-                    continue;
+        print(rowSums);
+        print(columnSums);
 
-                sum += arr[i][j];
+        for (Map.Entry<Long, Integer> entry : rowSums.entrySet()) {
+            if(columnSums.get(entry.getKey()) == null) {
+                return "Impossible";
             }
 
-            extra += container[i] - sum;
+            if (!columnSums.get(entry.getKey()).equals(entry.getValue())) {
+                return "Impossible";
+            }
         }
 
-        if(extra%2==0)
-            ans = "Possible";
-        else
-            ans = "Impossible";
+        return "Possible";
+    }
 
-        return ans;
+    private static void print(int[][] matrix) {
+        for (int row=0 ; row < matrix.length ; row++) {
+            for (int column=0 ; column < matrix[row].length ; column++) {
+                System.out.print(matrix[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void print (HashMap<Long, Integer> map) {
+        System.out.print("{ ");
+        for (Map.Entry<Long, Integer> entry : map.entrySet()) {
+            System.out.print("(" + entry.getKey() + " , " + entry.getValue() + ")");
+        }
+        System.out.println(" } ");
+    }
+
+    private static HashMap<Long, Integer> getColumnSums (int[][] matrix, int size) {
+        HashMap<Long, Integer> answer = new HashMap<>();
+        for (int column=0 ; column < size ; column++) {
+            long sum = sum(matrix, column);
+            answer.put(sum, answer.getOrDefault(sum, 0) + 1);
+        }
+        return answer;
+    }
+
+    private static HashMap<Long, Integer> getRowSums(int[][] container, int size) {
+        HashMap<Long, Integer> answer = new HashMap<>();
+        for (int row=0 ; row < size ; row++) {
+            long sum = sum(container[row]);
+            answer.put(sum, answer.getOrDefault(sum, 0) + 1);
+        }
+        return answer;
+    }
+
+    private static long sum(int[][] matrix, int column) {
+        int sum=0;
+        for (int[] aMatrix : matrix) {
+            sum += aMatrix[column];
+        }
+        return sum;
+    }
+
+    private static long sum (int[] array) {
+        int sum = 0;
+        for (int element : array) {
+            sum += element;
+        }
+        return sum;
+    }
+
+    private static void input(int[][] matrix) {
+        for (int index=0 ; index < matrix.length ; index++) {
+            for (int j=0 ; j < matrix[index].length ; j++) {
+                matrix[index][j] = scanner.nextInt();
+            }
+        }
     }
 }
