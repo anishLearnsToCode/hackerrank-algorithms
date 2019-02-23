9ed5cb3f1ce6d7167a78a7e89c3786e541f722b6
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-02-23 14:59:23 +0100

    solved acm icpc team

--- src/ACM_ICPC_TEAM.java
+++ src/ACM_ICPC_TEAM.java
@@ -13,29 +13,41 @@ public class ACM_ICPC_TEAM {
         System.out.print("\nEnter no. of subjects : ");
         m = s.nextInt();
 
-        boolean col[] = new boolean[m];
-        for(i=0 ; i<m ; col[i] = true, i++);
-
-        for(i=0 ; i<str.length ; i++){
+        for (i=0 ; i<size ; i++) {
             str[i] = s.next();
+        }
 
-            for(j=0 ; j<m ; j++){
-                if(col[j]){
-                    if(str[i].charAt(j)=='1')
-                        col[j] = false;
+        int answer[] = maxAcmTeams(str, m, str.length);
+        System.out.println(answer[0] + " " + answer[1]);
+    }
+
+    private static int[] maxAcmTeams(String[] people, int subjects, int contestants) {
+        int answer[] = new int[] {0, 0}, currentAbility;
+
+        for (int team1=0 ; team1 < contestants - 1 ; team1++) {
+            for (int team2=team1+1 ; team2 < contestants ; team2++) {
+                currentAbility = maxAbility(people[team1], people[team2], subjects);
+                if (currentAbility > answer[0]) {
+                    answer[0] = currentAbility;
+                    answer[1] = 1;
+                } else if (currentAbility == answer[0] ) {
+                    answer[1]++;
                 }
             }
         }
+        return answer;
+    }
 
-        for(i=0, c=0 ; i<col.length ; i++){
-            if(!col[i])
-                c++;
+    private static int maxAbility(String firstTeam, String secondTeam, int subjects) {
+        int answer = 0;
+        for (int index=0 ; index < subjects ; index++) {
+            answer += number(firstTeam.charAt(index)) | number(secondTeam.charAt(index));
         }
+        return answer;
+    }
 
-        System.out.println(c);
-
-        int ans = team(str, 0, str.length, str[0].length());
-        System.out.println(ans);
+    private static int number(char character) {
+        return (int)character - 48;
     }
 
     public static int team(String str[], int position, int n, int m){
