bfdc7b39b3182ef87d0154dd2b32d50a968ae3af
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-02-23 14:51:11 +0100

    solved forming a magic square

--- src/FormingAMagicSquare.java
+++ src/FormingAMagicSquare.java
@@ -1,15 +1,59 @@
 import java.util.Scanner;
 
 public class FormingAMagicSquare {
+    private static int[][][] allPossibilities = {
+            {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+            {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+            {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+            {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+            {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+            {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+            {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+            {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}
+    };
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int arr[][] = new int[3][3];
-        int ans;
+//        Scanner s = new Scanner(System.in);
+//        int arr[][] = new int[3][3];
+//        int ans;
 
-        arr_input2D(arr);
+//        arr_input2D(arr);
 
-        ans = magicSquare(arr);
-        System.out.println(ans);
+//        ans = magicSquare(arr);
+//        System.out.println(ans);
+
+        int square[][] = {
+                {4, 9, 2},
+                {3, 5, 7},
+                {8, 1, 5}
+        };
+
+        int square2[][] = {
+                {4, 8, 2},
+                {4, 5, 7},
+                {6, 1, 6}
+        };
+
+        System.out.println(formingMagicSquare(square));
+        System.out.println(formingMagicSquare(square2));
+    }
+
+    static int formingMagicSquare(int[][] square) {
+        int difference = Integer.MAX_VALUE;
+        for (int[][] possibility : allPossibilities) {
+            difference = Math.min(difference, difference(square, possibility));
+        }
+        return difference;
+    }
+
+    private static int difference(int square[][], int otherSquare[][]) {
+        int difference = 0;
+        for (int row=0 ; row < square.length ; row++) {
+            for (int column=0 ; column < square[row].length ; column++) {
+                difference += Math.abs(otherSquare[row][column] - square[row][column]);
+            }
+        }
+        return difference;
     }
 
     public static void arr_input2D(int arr[][]){
